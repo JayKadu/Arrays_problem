98cd06b0c39e3bb8b088933656622c41b3b223b9
Jay Kadu <96699153+JayKadu@users.noreply.github.com>
2024-03-07 15:15:46 +0530

    Update Binary_Search.java

--- Binary_Search.java
+++ Binary_Search.java
@@ -1,3 +1,4 @@
+//
 class Solution {
     int binarysearch(int arr[], int n, int k) {
         // code here
@@ -14,6 +15,27 @@ class Solution {
                 right=mid-1;
             }
             mid=(left+right)/2;
+        }
+        return -1;
+    }
+}
+// some modification: 
+class Solution {
+    int binarysearch(int arr[], int n, int k) {
+        // code here
+        int left=0;
+        int right=n-1;
+        int mid=left + (right - left)/2;
+        while(left<=right){
+            if(arr[mid]==k){
+                return mid;
+            }
+            if(k>arr[mid]){
+                left=mid+1;
+            }else{
+                right=mid-1;
+            }
+            mid=left + (right - left)/2;
         }
         return -1;
     }
