620df230b5735b60479fabecfb48123581d45cab
Jay Kadu <96699153+JayKadu@users.noreply.github.com>
2024-03-07 14:31:15 +0530

    Update Find_All_Duplicates_in_an_Array.java

--- Find_All_Duplicates_in_an_Array.java
+++ Find_All_Duplicates_in_an_Array.java
@@ -41,3 +41,20 @@ class Solution {
         arr[b]=temp;
     }
 }
+
+//method 3: using sort
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        Arrays.sort(nums);
+        List<Integer> list= new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if(i+1>=nums.length)
+                break;
+            if(nums[i]==nums[i+1]){
+                list.add(nums[i]);
+                i++;
+            }
+        }
+        return list;
+    }
+}
